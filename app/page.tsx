c645e2f2d474a63a5cf5fcc4ddd3b50ff73b5e5c
Sneyder Barreto <hello@sneyder.dev>
2025-06-06 16:30:37 -0500

    feat: add jest and rtl for testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
         <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-          Rick and Morty list
+          <h1 className="text-4xl font-bold">Rick and Morty list</h1>
         </main>
       </div>
     </div>

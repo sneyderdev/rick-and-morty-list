80d42f3164998924d8d0add618559d3c5da24b8a
Sneyder Barreto <hello@sneyder.dev>
2025-06-06 16:24:40 -0500

    chore: update ESLint config to include pnpm-lock.yaml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: ['.next/', 'node_modules/', 'dist/'],
+    ignores: ['.next/', 'node_modules/', 'dist/', 'pnpm-lock.yaml'],
   },
 ];
 

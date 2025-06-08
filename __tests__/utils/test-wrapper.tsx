7da1f654d3040e00255a0b551523aa40b97f17f7
Sneyder Barreto <hello@sneyder.dev>
2025-06-08 12:11:07 -0500

    test: remove page tests until finding out the issue with ESM and nuqs

--- __tests__/utils/test-wrapper.tsx
+++ __tests__/utils/test-wrapper.tsx
@@ -1,10 +1,5 @@
 import { BookmarksProvider } from '@/contexts/bookmarks-context';
-import { SearchProvider } from '@/contexts/search-context';
 
 export function TestWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <BookmarksProvider>
-      <SearchProvider>{children}</SearchProvider>
-    </BookmarksProvider>
-  );
+  return <BookmarksProvider>{children}</BookmarksProvider>;
 }
